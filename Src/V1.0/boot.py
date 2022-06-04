aa321bf61f6e79e52f510d103ccf6f5c40339198
PCX-LK <2105664367@qq.com>
2022-06-04 11:44:34 +0800

    Fix BOOT.PY

--- Src/V1.0/boot.py
+++ Src/V1.0/boot.py
@@ -1,6 +1,6 @@
 from machine import UART,Pin
 import time
-#Ver1.0
+#
 uart = UART(2, baudrate=9600, tx=17, rx=16)
 sw1 = Pin(27, Pin.IN, Pin.PULL_UP)
 sw2 = Pin(26, Pin.IN, Pin.PULL_UP)
@@ -9,29 +9,52 @@ sw4 = Pin(32, Pin.IN, Pin.PULL_UP)
 bk = Pin(25, Pin.IN, Pin.PULL_UP)
 l = Pin(2, Pin.OUT)
 
-k1s = k2s = k3s = k4s = 0x00
 a = 0
-kso = [k1s,k2s,k3s,k4s]
+k1s = k2s = k3s = k4s = 0x00
+k1ms = k2ms = k3ms = k4ms = 0
 Sum = 0x0c
+kso = [k1s,k2s,k3s,k4s]
+
+#k1s = 
+#k2s = 
+#k3s = 
+#k4s = 
 
 while a == 0 :
-    k1 = k2 = k3 = k4 = b'\x00'
-    k1s = k2s = k3s = k4s = 0x00
-    
     if bk.value() == 0 :
         break
     if sw1.value() == 0 :
         k1 = b'\x07'
         k1s = 0x07
+        k1ms = time.ticks_ms()
+    elif time.ticks_diff(time.ticks_ms(), k1ms) >= 5 :
+        k1 = b'\x00'
+        k1s = 0x00
+
     if sw2.value() == 0 :
         k2 = b'\x09'
         k2s = 0x09
+        k2ms = time.ticks_ms()
+    elif time.ticks_diff(time.ticks_ms(), k2ms) >= 5 :
+        k2 = b'\x00'
+        k2s = 0x00
+
     if sw3.value() == 0 :
         k3 = b'\x0d'
         k3s = 0x0d
+        k3ms = time.ticks_ms() 
+    elif time.ticks_diff(time.ticks_ms(), k3ms) >= 5 :
+        k3 = b'\x00'
+        k3s = 0x00
+    
     if sw4.value() == 0 :
         k4 = b'\x0e'
         k4s = 0x0e
+        k4ms = time.ticks_ms()
+    elif time.ticks_diff(time.ticks_ms(), k4ms) >= 5 :
+        k4 = b'\x00'
+        k4s = 0x0e
+
     ks = [k1s,k2s,k3s,k4s]
     SumR = chr(k1s + k2s + k3s + k4s + Sum)
     keys = b'\x57\xab\x00\x02\x08\x00\x00' + k1 + k2 + k3 + k4 + b'\x00\x00' + SumR
